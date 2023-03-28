b4b9a48cbdc34b0447935910f8c6c620e157a510
zzuzzu0205 <zkakcnd@naver.com>
2023-03-28 23:41:18 +0900

    'state'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import { Routes, Route, Link } from "react-router-dom";
+import { getCookie } from "./cookie";
 
 function App() {
+  const [mode, setMode] = useState("welcome");
+  const [id, setId] = useState(null);
+  let content = null;
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "js", body: "js is ..." },
+  ];
+  if (mode === "welcome") {
+    content = <Article title="welcome" body="hello world" />;
+  } else if (mode === "read") {
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        content = <Article title={topics[i].title} body={topics[i].body} />;
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Head
+        title="WEB"
+        onChangeMode={() => {
+          setMode("welcome");
+        }}
+      />
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          setMode("read");
+          setId(id);
+        }}
+      />
+      {content}
     </div>
+  );
+}
+function Head(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChangeMode(Number(e.target.id));
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
   );
 }
 
