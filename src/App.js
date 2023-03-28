772bef77128986a009e07ecec518691cd55f8d4d
zzuzzu0205 <zkakcnd@naver.com>
2023-03-29 00:18:25 +0900

    'update'

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import io from "socket.io-client";
 import { Routes, Route, Link } from "react-router-dom";
-import { getCookie } from "./cookie";
 
 function App() {
   const [mode, setMode] = useState("welcome");
   const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
   let content = null;
-  const topics = [
+  const [topics, setTopices] = useState([
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "js", body: "js is ..." },
-  ];
+  ]);
   if (mode === "welcome") {
     content = <Article title="welcome" body="hello world" />;
   } else if (mode === "read") {
@@ -21,6 +21,20 @@ function App() {
         content = <Article title={topics[i].title} body={topics[i].body} />;
       }
     }
+  } else if (mode === "create") {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          const newTopic = { id: nextId, title: _title, body: _body };
+          const newTopics = [...topics];
+          newTopics.push(newTopic);
+          setTopices(newTopics);
+          setMode("read");
+          setId(nextId);
+          setNextId(nextId + 1);
+        }}
+      />
+    );
   }
 
   return (
@@ -39,6 +53,15 @@ function App() {
         }}
       />
       {content}
+      <a
+        href="/create"
+        onClick={(e) => {
+          e.preventDefault();
+          setMode("create");
+        }}
+      >
+        create
+      </a>
     </div>
   );
 }
@@ -94,4 +117,29 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const title = e.target.title.value;
+          const body = e.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title!" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body!!"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
 export default App;
