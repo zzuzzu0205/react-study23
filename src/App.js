8b0c6fe9caba35f44f0807951704d888e252d51e
zzuzzu0205 <zkakcnd@naver.com>
2023-05-05 16:33:38 +0900

    'nestRoute'

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Link, Route, Routes, NavLink } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes, NavLink ,useParams} from "react-router-dom";
 
 function Home() {
   return (
@@ -12,16 +12,42 @@ function Home() {
     </div>
   );
 }
-
+const content = [
+  {id:1, title:"HTML",des:"HTML is..."},
+  {id:2, title:"CSS",des:"CSS is..."},
+  {id:3, title:"JS",des:"JS is..."}
+]
 function Topics() {
+ 
+
   return (
     <div>
       <h2>Topics</h2>
-      Topics...
+      {
+        content.map((a,i)=>{
+          return(
+            <ul key={content[i].id}>
+              <li><NavLink  to={"/topics/" + content[i].id}>{content[i].title}</NavLink></li>
+            </ul>
+          )})
+      }
+      
+      
     </div>
   );
 }
-
+function Topic(){
+  let {topic_id} = useParams();
+  
+  
+  return(
+    <div>
+      <Topics/>
+      <h3>Topic</h3>
+      <p>{content[topic_id -1].des}</p>
+    </div>
+  )
+}
 function Contact() {
   return (
     <div>
@@ -35,6 +61,7 @@ function App() {
   return (
     <div>
       <h1>Hello React Router DOM</h1>
+      
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -49,8 +76,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/topics" element={<Topics />} />
+        
+        <Route path="/topics/:topic_id" element={<Topic/>}></Route>
         <Route path="/contact" element={<Contact />} />
         <Route path="/*" element={<div>없는 페이지입니다</div>} />
+        
       </Routes>
     </div>
   );
